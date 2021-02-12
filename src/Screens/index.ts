daabb2c3d05124a920a79bf5116b25d30d9c4d4a
Caelin Sutch <caelinsutch@gmail.com>
2021-02-11 23:04:23 -0800

    Fix build problem

--- src/Screens/index.ts
+++ src/Screens/index.ts
@@ -1,4 +1,3 @@
 export { default as HomeScreen } from './HomeScreen';
 export { default as SubmitScreen } from './SubmitScreen';
 export { default as AboutScreen } from './AboutScreen';
-export type { AboutScreenProps } from './AboutScreen';
