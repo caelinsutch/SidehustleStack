dabcbebe870d54e8a850828b7484d403f8b96b28
Stephen Krider <skrider@berkeley.edu>
2021-02-17 01:38:47 -0800

    implements PlatformCard and VoteIcons

---
+++ src/Components/VoteIcons/VoteIcons.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export type VoteIconsProps = {}
+
+const VoteIcons: React.FC<VoteIconsProps> = (props) => {
+  return (
+    <div>
+      VoteIcons
+    </div>
+  );
+};
+
+export default VoteIcons;
---
+++ src/Components/VoteIcons/index.tsx
@@ -0,0 +1,2 @@
+export { default } from './VoteIcons';
+export type { VoteIconsProps } from './VoteIcons';
