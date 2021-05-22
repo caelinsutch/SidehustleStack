17db1533961708fc4a2072a8f2aaa0faa71d649f
Caelin Sutch <caelinsutch@gmail.com>
2021-05-22 14:01:17 -0700

    Deploy frontend changes

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -44,41 +44,41 @@ const getAllNames = gql`
   }
 `;
 
-// export const getStaticPaths: GetStaticPaths = async () => {
-//   const platforms: GetAllNamesQuery = (
-//     await client.query({
-//       query: getAllNames,
-//     })
-//   ).data;
-//
-//   const paths = platforms.allPlatforms.map((platform) => ({
-//     params: {
-//       id: parsePlatformNameForUrl(platform.name),
-//     },
-//   }));
-//
-//   return { paths, fallback: false };
-// };
+export const getStaticPaths: GetStaticPaths = async () => {
+  const platforms: GetAllNamesQuery = (
+    await client.query({
+      query: getAllNames,
+    })
+  ).data;
 
-// export const getStaticProps: GetStaticProps = async ({ params }) => {
-//   const { id } = params;
-//
-//   const name = parsePlatformNameFromUrl(id as string);
-//
-//   const { data } = await client.query({
-//     query: getPlatformQuery,
-//     variables: {
-//       name,
-//     },
-//   });
-//
-//   return {
-//     props: {
-//       data,
-//       id: data.platform.id,
-//     },
-//   };
-// };
+  const paths = platforms.allPlatforms.map((platform) => ({
+    params: {
+      id: parsePlatformNameForUrl(platform.name),
+    },
+  }));
+
+  return { paths, fallback: false };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { id } = params;
+
+  const name = parsePlatformNameFromUrl(id as string);
+
+  const { data } = await client.query({
+    query: getPlatformQuery,
+    variables: {
+      name,
+    },
+  });
+
+  return {
+    props: {
+      data,
+      id: data.platform.id,
+    },
+  };
+};
 
 const Platform: React.FC<{ data: GetPlatformQuery; id: string }> = ({
   data,
@@ -87,9 +87,9 @@ const Platform: React.FC<{ data: GetPlatformQuery; id: string }> = ({
   <PageWrapper
     backgroundImage="url('../platform-profile-background.png')"
     backgroundSize="contain"
-    // title={`${data.platform.name} - Sidehustle Stack`}
+    title={`${data.platform.name} - Sidehustle Stack`}
   >
-    {/* <PlatformScreen data={data} id={id} /> */}
+    <PlatformScreen data={data} id={id} />
   </PageWrapper>
 );
 
